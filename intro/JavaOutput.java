58e84683268b62595dd4d1439301131d7b78ad08
Cchrisekwugum <cchrisekwugum@gmail.com>
2026-02-14 15:04:19 +0100

    Added a new fil for ooutput

---
+++ intro/JavaOutput.java
@@ -0,0 +1,11 @@
+public class JavaOutput {
+    public static  void main(String[] args){
+        // Output statements in java
+        // this is use to print a message to the console just like the normal print statement in python or console log in javascript
+        System.out.println("\n1. Hello here, I am writing python program in java\n");
+        System.out.println("\n2. This is my first java program, but not really my first in programming language.");
+        System.out.println("\n3. I am learning java to be a better programmer and to expand my knowledge in programming languages.");
+        System.out.println("\n4. I am excited to learn java and to see what I can do with it in the future.");
+
+    }
+}
